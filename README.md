1a2deb72d4d5cd003c70974eba0ba20d5a0b1b72
Camilo Barraza <cbarraza11personal@gmail.com>
2022-04-27 17:58:36 -0500

    update example code

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # fuse-ts-vue
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### Running importer
+
+Example available in src/App.vue
+
+### Running importer based on CDN
+
+Requires adding `<script src="https://unpkg.com/fuse-importer@0.18.0"></script>` to **public/index.html**
--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,14 @@
 <template>
-  <!-- <button @click="showImporter">Import</button> -->
+  <button @click="showImporter">Show Importer</button>
+  <button @click="showCDNBasedImporter">Show CDN Based Importer</button>
   <!-- mount the component button -->
-  <button @click="mountRC">mount rc</button>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import { DateTime } from "luxon";
-// import FuseImporter from "fuse-importer";
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-// var sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-// const importer = new FuseImporter("2DXqRB8ueCyqFkQwszyIVw", "180", {
-//   env: "development",
-// });
-
-// importer.onSubmit = async (reviewedData) => {
-//   // simulate api call delay
-//   await sleep(2000);
-//   console.log({ reviewedData });
-
-//   // simulate random errors on submitted data
-//   const validationErrors = {};
-//   const maxErrorCount = 6;
-
-//   const errorCount = Math.round(
-//     Math.random() * Math.min(reviewedData.length, maxErrorCount)
-//   );
-//   for (let i = 0; i < errorCount; i++) {
-//     const errorIndex = Math.round(Math.random() * reviewedData.length);
-//     if (errorIndex < reviewedData.length) {
-//       const recordFields = Object.keys(reviewedData[0]).filter(
-//         (r) => r !== "_meta"
-//       );
-//       const fieldWithErrorIndex = Math.floor(
-//         Math.random() * recordFields.length
-//       );
-//       validationErrors[reviewedData[errorIndex]._meta.id] = {
-//         [recordFields[fieldWithErrorIndex]]: "Duplicate value",
-//       };
-//     }
-//   }
-
-//   let message = null;
-//   if (!Object.keys(validationErrors).length)
-//     message = "Data imported successfully";
-//   else message = "Invalid records identified when importing to database";
-
-//   return {
-//     message,
-//     errors: validationErrors,
-//   };
-// };
-
-// const onValidateRecord = async () => {
-//   const errors = {};
-//   return errors;
-// };
-
-// importer.onValidateRecord = onValidateRecord;
-
-const res = DateTime.now()
-  .setZone("America/New_York")
-  .minus({ weeks: 1 })
-  .endOf("day")
-  .toISO();
-console.log({ res });
-
-const TestComponent = () => {
-  return /*#__PURE__*/ React.createElement("div", null, "testing firme");
-};
+import FuseImporter from "fuse-importer";
 
 export default {
   name: "App",
@@ -80,29 +16,50 @@ export default {
     HelloWorld,
   },
   methods: {
-    // showImporter() {
-    //   importer.show();
-    // },
-    mountRC() {
-      const div = document.createElement("div");
-      div.setAttribute("id", "idtest");
-      div.innerHTML = "test";
-      document.body.appendChild(div);
+    showCDNBasedImporter() {
+      const apiToken = "2DXqRB8ueCyqFkQwszyIVw";
+      const templateId = "180";
+      const FuseImporter = window.FuseImporter;
+      const importer = new FuseImporter(apiToken, templateId, {
+        env: "development",
+      });
 
-      const container = document.getElementById("idtest");
-      const root = createRoot(container);
-      root.render(
-        React.createElement(TestComponent, {
-          test: "test",
-        })
-      );
+      importer.onSubmit = async (reviewedData) => {
+        console.log({ reviewedData });
+        return {
+          errors: {},
+          message: "Data imported successfully",
+        };
+      };
 
-      // ReactDOM.createRoot(
-      //   React.createElement(TestComponent, {
-      //     test: "test",
-      //   }),
-      //   document.getElementById("idtest")
-      // );
+      const onValidateRecord = async () => {
+        return {};
+      };
+      importer.onValidateRecord = onValidateRecord;
+
+      importer.show();
+    },
+    showImporter() {
+      const apiToken = "2DXqRB8ueCyqFkQwszyIVw";
+      const templateId = "180";
+      const importer = new FuseImporter(apiToken, templateId, {
+        env: "development",
+      });
+
+      importer.onSubmit = async (reviewedData) => {
+        console.log({ reviewedData });
+        return {
+          errors: {},
+          message: "Data imported successfully",
+        };
+      };
+
+      const onValidateRecord = async () => {
+        return {};
+      };
+      importer.onValidateRecord = onValidateRecord;
+
+      importer.show();
     },
   },
 };
